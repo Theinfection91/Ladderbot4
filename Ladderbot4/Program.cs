253d54c130a080535fe01a30b00dbe127b2a7e26
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2024-12-14 10:58:16 -0500

    test again

--- Ladderbot4/Program.cs
+++ Ladderbot4/Program.cs
@@ -16,7 +16,6 @@ namespace Ladderbot4
 {
     public class Program
     {
-        // new pc test
         private DiscordSocketClient _client;
         private CommandService _commands;
         private IServiceProvider _services;
